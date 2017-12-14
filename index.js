82f15f466076cc0f3f3740d26dbb06d166aaee27
Aubree Abril <aubree.abril@gmail.com>
2017-12-14 02:43:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments){
+  const arr = []
+  for (let i = 0, 1 = musicians.length; i <1; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr
+}
+
